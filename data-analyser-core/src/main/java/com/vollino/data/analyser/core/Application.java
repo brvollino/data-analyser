af748d1e61ee98c046c407e87d14757c85ba1deb
Bruno <brvollino@gmail.com>
2018-08-05 12:07:53 -0300

    Creates gradle project with Spring boot basic app

---
+++ data-analyser-core/src/main/java/com/vollino/data/analyser/core/Application.java
@@ -0,0 +1,10 @@
+package com.vollino.data.analyser.core;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Bruno Vollino
+ */
+@SpringBootApplication
+public class Application {
+}
---
+++ data-analyser-core/src/test/java/com/vollino/data/analyser/core/ApplicationIT.java
@@ -0,0 +1,18 @@
+package com.vollino.data.analyser.core;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Bruno Vollino
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationIT {
+
+    @Test
+    public void shouldBootApplication() {
+    }
+}
